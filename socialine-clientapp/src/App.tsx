05be746aaa3e6628c5b1b17a74d19f1c800cebc2
BilgesPage <cbpamukcu@gmail.com>
2020-04-29 19:11:54 +0100

    Commit003: Walking Skeleton With Semantic UI Test

--- socialine-clientapp/src/App.tsx
+++ socialine-clientapp/src/App.tsx
@@ -2,20 +2,9 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import  {Header, Icon} from 'semantic-ui-react'
 
 class App extends Component {
-
-  /* SAMPLE1: PASSING SAMPLE DATA INTO THE BROWSER
-  state= {
-    values: []
-  }
-
-  componentDidMount() {
-    this.setState({
-      values: [{id:1, name: 'Value 101'}, {id:2, name: 'Value 102'} ]
-    })
-  };
-  */
 
   state = {
     values : []
@@ -37,43 +26,19 @@ class App extends Component {
 
   render(){
     return (
+      <div>
+        <Header as='h2'>
+          <Icon name='users'/>
+          <Header.Content>SociaLINE</Header.Content>
+        </Header>
 
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          {/* <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a> */}
-
-          {
-          /* SAMPLE1: PASSING SAMPLE DATA INTO THE BROWSER
-          <ul>
-            {
-              this.state.values.map((value: any) => (
-              <li>{value.name}</li>
-              ))
-            }
-          </ul>
-          */
-          }
-
-          <ul>
-            {
-              this.state.values.map( (value:any) => (
-              <li key={value}>{value.name}</li>
-              ))
-            }
-          </ul>
-
-        </header>
+        <ul>
+        {
+          this.state.values.map( (value:any) => (
+          <li key={value}>{value.name}</li>
+          ))
+        }
+        </ul>
       </div>
     );
   }; 
